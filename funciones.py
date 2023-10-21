2223d39adfed92095963c33e15ba606df35091d5
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2023-10-21 19:41:57 -0300

    Add files via upload

--- funciones.py
+++ funciones.py
@@ -1,4 +1,8 @@
 import datetime
+import pandas as pd
+import numpy as np
+
+# Funciones para encontrar palabras y numeros repetidos
 
 def contar_valor(lista, numero):
     contador = 0
@@ -14,7 +18,40 @@ def contar_cadena(lista, cadena):
             contador += 1
     return contador
 
+
+# Funcion para calcular la edad de una persona
+
 def calcular_edad(fecha_nacimiento):
     fecha_nacimiento = datetime.datetime.strptime(fecha_nacimiento, '%d-%m-%Y')
     edad = datetime.datetime.now().year - fecha_nacimiento.year
     return edad
+
+
+# Funciones para manejar Data Frames
+
+def promedio_df(data_frame, columna):
+    data_frame = pd.DataFrame(data_frame)
+    if columna in data_frame.columns:
+        promedio = data_frame[columna].mean()
+    else: 
+        promedio = 0
+        print(f'La columna {columna} no fue encontrada')
+    return promedio
+
+def contar_df(data_frame):
+    data_frame = pd.DataFrame(data_frame)
+    repeticiones = {}
+    for columna in data_frame:
+        for valor in data_frame[columna]:
+            if valor in repeticiones:
+                repeticiones[valor] += 1
+            else: repeticiones[valor] = 1
+    return repeticiones
+
+def cadenas_minus_df(data_frame):
+    data_frame = pd.DataFrame(data_frame)
+    df_minus = data_frame.copy()
+    for columna in data_frame.columns:
+        if data_frame[columna].dtype == 'object':
+            df_minus[columna] = data_frame[columna].apply(str).str.lower()
+    return df_minus
