24b8fcf77e98deb4de86c124c3387adabd0d4aac
Agustin Demarco <123703934+aagusdemarco@users.noreply.github.com>
2023-11-20 18:48:07 -0300

    Add files via upload

---
+++ emails.py
@@ -0,0 +1,22 @@
+mensajes = {
+    'bienvenida': 
+    {
+        'asunto': 'Bienvenida',
+        'mensaje': f'¡Querido/a {socio["nombre"]}!\n\nEn nombre de todos nosotros en el Club de Futbol Los Tigres, te damos una cálida bienvenida. Estamos emocionados de tenerte como nuevo socio. Tu decisión de unirte a nuestra familia futbolística significa mucho para nosotros. Esperamos que tu experiencia sea llena de emociones, camaradería y, por supuesto, muchos goles. Siéntete libre de explorar todo lo que nuestro club tiene para ofrecer\n¡Vamos Tigres!'
+    },
+    'fixture': 
+    {
+        'asunto': 'Próximos Partidos',
+        'mensaje': f'¡Hola {socio["nombre"]}!\n\nNos complace informarte sobre los emocionantes partidos que el Club de Futbol Los Tigres tiene programados para los próximos meses. Desde enfrentamientos en nuestra casa hasta épicas batallas en campos rivales, cada partido es una oportunidad para fortalecer nuestro lazo como comunidad. Esperamos verte en las gradas, alentando con pasión y celebrando cada victoria. ¡Prepárate para una temporada llena de emoción y adrenalina!\n¡Vamos Tigres!'
+    },
+    'promociones': 
+    {
+        'asunto': 'Promociones',
+        'mensaje': f'Estimado/a {socio["nombre"]},\n\nAgradecemos tu lealtad y apoyo continuo al Club de Futbol Los Tigres. Como muestra de nuestro agradecimiento, hemos preparado promociones exclusivas para ti. Desde descuentos en mercancía del club hasta ofertas especiales en entradas, queremos asegurarnos de que tu experiencia como socio sea aún más gratificante. ¡No te pierdas estas increíbles oportunidades y continúa siendo parte de nuestra historia!\n¡Vamos Tigres!'
+    },
+    'eventos': 
+    {
+        'asunto': 'Eventos',
+        'mensaje': f'¡Hola {socio["nombre"]}!\n\nTe extendemos una cordial invitación a los eventos especiales que el Club de Futbol Los Tigres ha organizado exclusivamente para nuestros socios. Desde encuentros con jugadores hasta sesiones de entrenamiento exclusivas, estamos emocionados de ofrecerte experiencias únicas que van más allá del campo de juego. Esperamos que aproveches al máximo tu membresía participando en estas oportunidades de conexión y celebración. ¡Esperamos verte pronto!\n¡Vamos Tigres!'
+    }
+}
